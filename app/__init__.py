1ed012b642eb66c458a7193cb77ad7abfc217641
denialanderror <denialanderror@googlemail.com>
2015-07-22 14:33:28 +0100

    HTML error handling pages added. Push to server to check if Redis works.

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from redis import Redis
 
 app = Flask(__name__)
 app.config.from_object('config')
-redis = Redis(host=os.getenv('REDIS_URL', 'localhost'), port=os.getenv('REDIS_PORT_6379_TCP_PORT', 6379), db=0,
+redis = Redis(host=os.getenv('REDIS_URL', 'localhost'), db=0,
               charset="utf-8", decode_responses=True)
 # db = SQLAlchemy(app)
 
