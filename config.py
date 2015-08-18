104863d2c5d3a44dedbcbcc8b0538306e57096ad
denialanderror <denialanderror@googlemail.com>
2015-08-18 11:55:43 +0100

    - Evaluation methods added (feedback page and rating stars) - MongoDB fully implemented and moved to MongoLab cloudhosting for production and to allow testing - Action and reaction emotional responses written but not implemented

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-WTF_CSRF_ENABLED = True
-MONGODB_SETTINGS = {'db': "bedtime"}
-SECRET_KEY = 'changeme'
+MONGODB_SETTINGS = {'db': 'bedtime',
+                    'host': 'mongodb://denialanderror:e806912g@ds055792.mongolab.com:55792/bedtime'}
 
-# needed for SQLAlchemy and database migration
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# # needed for SQLAlchemy and database migration
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # needed for WTForm validation
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
 # needed for email
 ADMIN = ['denialanderror@googlemail.com']
 SERVER = 'localhost'
