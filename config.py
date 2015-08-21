a4df9233aa82a80b004b7b2b5f3ec80bd3476115
denialanderror <denialanderror@googlemail.com>
2015-08-21 17:44:12 +0100

    - passwords removed from config file (and changed!)

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # remember to remove login information from VCS pushes!
 MONGODB_SETTINGS = {'db': 'bedtime',
-                    'host': 'mongodb://denialanderror:e806912g@ds055792.mongolab.com:55792/bedtime'}
+                    'host': 'MONGODB_URI'}
 
 # needed for WTForm validation
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = 'password'
 
 # needed for email
 ADMIN = ['denialanderror@googlemail.com']
