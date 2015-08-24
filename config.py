eaa27f141af5d24a37a794cd67fe694def7c1add
denialanderror <denialanderror@googlemail.com>
2015-08-24 13:38:52 +0100

    - Can't work out how to do this bloody environment variables thing so screw it, have my passwords!

--- config.py
+++ config.py
@@ -4,13 +4,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 # remember to remove login information from VCS pushes!
+# MONGODB_SETTINGS = {'db': 'bedtime',
+#                     'host': os.environ('MONGODB_URI')}
+
 MONGODB_SETTINGS = {'db': 'bedtime',
-                    'host': os.environ('MONGODB_URI')}
+                    'host': 'mongodb://denialanderror:guest001@ds055862.mongolab.com:55862/bedtime'}
 
 
 # needed for WTForm validation
 WTF_CSRF_ENABLED = True
-SECRET_KEY = os.environ('FLASK_KEY')
+SECRET_KEY = 'password'
 
 # needed for email
 ADMIN = ['denialanderror@googlemail.com']
