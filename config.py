edfed55f2532b8c63a422845126b85496f66d7ea
denialanderror <denialanderror@googlemail.com>
2015-08-24 11:47:05 +0100

    - Templating for HTML rearranged and simplified - Contribute works and adds user-generated content to temp Mongo document

--- config.py
+++ config.py
@@ -2,17 +2,15 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# MONGODB_SETTINGS = {'db': 'bedtime'}
-
-# remember to remove login information from VCS pushes!
-MONGODB_SETTINGS = {'db': 'bedtime',
-                    'host': 'MONGODB_URI'}
-
-# needed for WTForm validation
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'PASSWORD'
-
 # needed for email
 ADMIN = ['denialanderror@googlemail.com']
 SERVER = 'localhost'
 PORT = '25'
+
+MONGODB_SETTINGS = {'db': 'bedtime',
+                    'host': 'MONGODB_URI'}
+
+
+# needed for WTForm validation
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'PASSWORD'
